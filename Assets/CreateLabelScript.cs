081de4a82f51c92a8a075bad6535d5510710c9ab
chenlitao <269391704@qq.com>
2015-08-29 21:32:14 +0800

    修改readme，增加代码注释

--- Assets/CreateLabelScript.cs
+++ Assets/CreateLabelScript.cs
@@ -19,24 +19,31 @@ public class CreateLabelScript : MonoBehaviour
 	
 	}
 
+	/**
+	 * 创建一个Label
+	*/
 	void LO_CreateLabel ()
 	{
 		GameObject lab = NGUITools.AddChild (root);
 		UILabel label = lab.AddComponent<UILabel> ();
+		//设置Label的文字
 		label.text = "LanOuKeji";
+		//设置Label的字体颜色
 		label.color = Color.green;
-
+		//设置Label的字体
 //		Font trueFont = Font.CreateDynamicFontFromOSFont ("Calibri", 20);
 //		label.trueTypeFont = trueFont;
 
-
+		//设置Label的字体
 		UIFont ft = fontPrefab.GetComponent<UIFont> ();
 		label.bitmapFont = ft;
 	
+		//设置Label的宽度和高度
 		label.width = 200;
 		label.height = 100;
-
+		//设置Label的尺寸根据字体的变化而变化
 		label.overflowMethod = UILabel.Overflow.ResizeFreely;
+		//设置Label文字有阴影效果
 		label.effectStyle = UILabel.Effect.Shadow;
 
 	}
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # LO_UILabelLession
+
+## 通过代码讲解了NGUI中UILabel的实现方式和常用属性的设置
+
+###关键代码
+    void LO_CreateLabel ()
+	{
+		GameObject lab = NGUITools.AddChild (root);
+		UILabel label = lab.AddComponent<UILabel> ();
+		//设置Label的文字
+		label.text = "LanOuKeji";
+		//设置Label的字体颜色
+		label.color = Color.green;
+		//设置Label的字体
+		//Font trueFont = Font.CreateDynamicFontFromOSFont ("Calibri", 20);
+		//label.trueTypeFont = trueFont;
+
+		//设置Label的字体
+		UIFont ft = fontPrefab.GetComponent<UIFont> ();
+		label.bitmapFont = ft;
+	
+		//设置Label的宽度和高度
+		label.width = 200;
+		label.height = 100;
+		//设置Label的尺寸根据字体的变化而变化
+		label.overflowMethod = UILabel.Overflow.ResizeFreely;
+		//设置Label文字有阴影效果
+		label.effectStyle = UILabel.Effect.Shadow;
+	}
+		
