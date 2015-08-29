545acf5597fe89264074fcf95f3b50ba8c6c4f40
chenlitao <269391704@qq.com>
2015-08-29 19:57:35 +0800

    first commit

---
+++ Assets/CreateLabelScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateLabelScript : MonoBehaviour
+{
+
+	public GameObject root;
+
+	public GameObject fontPrefab;
+	// Use this for initialization
+	void Start ()
+	{
+		LO_CreateLabel ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void LO_CreateLabel ()
+	{
+		GameObject lab = NGUITools.AddChild (root);
+		UILabel label = lab.AddComponent<UILabel> ();
+		label.text = "LanOuKeji";
+		label.color = Color.green;
+
+//		Font trueFont = Font.CreateDynamicFontFromOSFont ("Calibri", 20);
+//		label.trueTypeFont = trueFont;
+
+
+		UIFont ft = fontPrefab.GetComponent<UIFont> ();
+		label.bitmapFont = ft;
+	
+		label.width = 200;
+		label.height = 100;
+
+		label.overflowMethod = UILabel.Overflow.ResizeFreely;
+		label.effectStyle = UILabel.Effect.Shadow;
+
+	}
+}
